e51a8afcf3af618387610eb63ab8ba833ee8da7e
farzamshahid <farzamshahid6@gmail.com>
2024-04-17 17:19:21 +0500

    change

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,3 +1,4 @@
+import { arrowRight } from "../assets/icons"
 import { shoe8 } from "../assets/images";
 import Button from "../components/Button";
 const SuperQuality = () => {
@@ -21,7 +22,7 @@ const SuperQuality = () => {
                     Our dedication to detail and excellence ensures your satisfaction
                 </p>
                 <div className='mt-11'>
-                    <Button label='View details' />
+                    <Button label='View details' iconURL={arrowRight} />
                 </div>
             </div>
 
